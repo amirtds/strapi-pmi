e314f639251e076298f176ca83193d73828c2066
Amir Tadrisi <amirtds@gmail.com>
2022-06-18 22:55:30 -0500

    chore: add gql plugins config

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = {
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+    },
+},
+};
